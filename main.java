34507ba6e70c7de3fa753952fc47253a2ed49173
Diego Santiago Barbosa Galarza <dibaos2007@gmail.com>
2026-03-27 21:04:46 -0500

    Create main.java
    agregue los 12 caballeros del proyecto que se hizo en clases virtuales en inteligent, sin embargo no logre que se replicara el proyecto en este repo también, el archivo completo esta en mi perfil de GitHub si le gustaría verlo

---
+++ main.java
@@ -0,0 +1,65 @@
+
+//aqui estan los 12 cablleros del zodiaco de la clase virtual, al estar en otro repo no lo pude poner aqui, sin embargo en mi perfil esta el repositorio con todas la clases y objetos que creamos en clase, aqui solo estan los caballeros si embargo falta lo demas para que salga correctamente,espero me pueda entender 
+public class Main {
+    public static void main(String[] args) {
+
+
+        Persona p1 = new Persona("Yonaiker");
+        Persona p2 = new Persona("MILLOS DAVID");
+        Persona p3 = new Persona("TAL CUAL");
+
+        Caballero c1 = new Caballero("Mu DE ");
+        c1.constelacion="Aries";
+        c1.material=Material.ORO;
+        c1.dios=Dios.ATENEA;
+        //aqui puse a saga y kanon
+        Caballero c3 = new Caballero("saga y kanon de ");
+        c3.constelacion="geminis";
+        c3.material=Material.ORO;
+        c3.dios=Dios.ATENEA;
+        //AQUI ESTA CANCER (MUERTE MASK)
+        Caballero c4 = new Caballero("MUERTE MASK de ");
+        c4.constelacion="CANCER";
+        c4.material=Material.ORO;
+        c4.dios=Dios.ATENEA;
+        //AQUI ESTA LEO(AIORIA)
+        Caballero c5 = new Caballero("AIORIA de ");
+        c5.constelacion="LEO";
+        c5.material=Material.ORO;
+        c5.dios=Dios.ATENEA;
+        //AQUI ESTA VIRGO(SAKHA)
+        Caballero c6 = new Caballero("SHAKA de ");
+        c6.constelacion="VIRGO";
+        c6.material=Material.ORO;
+        c6.dios=Dios.ATENEA;
+        //AQUI ESTA LIBRA(DOHKO)
+        Caballero c7 = new Caballero("DOCKO de ");
+        c7.constelacion="LIBRA";
+        c7.material=Material.ORO;
+        c7.dios=Dios.ATENEA;
+        //AQUI ESTA ESCORPIO(MILO)
+        Caballero c8 = new Caballero("MILO de ");
+        c8.constelacion="ESCORPIO";
+        c8.material=Material.ORO;
+        c8.dios=Dios.ATENEA;
+        //AQUI ESTA SAGITARIO(AIOROS)
+        Caballero c9 = new Caballero("AIOROS de ");
+        c9.constelacion="SAGITARIO";
+        c9.material=Material.ORO;
+        c9.dios=Dios.ATENEA;
+        //AQUI ESTA capricornio(shura)
+        Caballero c10 = new Caballero("shura de ");
+        c10.constelacion="CAPRICORNIO";
+        c10.material=Material.ORO;
+        c10.dios=Dios.ATENEA;
+        //AQUI ESTA ACUARIO(CAMUS)
+        Caballero c11 = new Caballero("CAMUS de ");
+        c11.constelacion="ACUARIO";
+        c11.material=Material.ORO;
+        c11.dios=Dios.ATENEA;
+        //AQUI ESTA PICIS(AFRODITA)
+        Caballero c12 = new Caballero("AFRODITA de ");
+        c12.constelacion="PICIS";
+        c12.material=Material.ORO;
+        c12.dios=Dios.ATENEA;
+
